b5e8ade59f812f7fe5fc95c76fb4d6c9e17ecff5
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2022-03-18 12:43:29 -0300

    Trabajando con imports y exports.

---
+++ src/bases/08-imp-exp.js
@@ -0,0 +1,13 @@
+import heroes, {owners} from "../data/heroes";
+
+const getHeroById = (id) => {
+    return heroes.find(heroe => heroe.id === id);
+}
+
+console.log(getHeroById(2));
+
+const getHeroByOwner = (owner) => heroes.filter(heroe => heroe.owner === owner);
+
+console.log(getHeroByOwner("DC"));
+
+console.log(owners);
