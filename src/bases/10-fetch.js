a913e8ee9c596446a7dfd6f4ea6e32d8900d5de0
Marcelo Cartagena <marcelocartagenaorellana@gmail.com>
2022-03-29 12:39:33 -0300

    Trabajando con fetch.

---
+++ src/bases/10-fetch.js
@@ -0,0 +1,18 @@
+const apiKey = 'nnf90kUoJAbaBM90YzdM0b6PVEUJzD2B';
+
+const peticion = fetch(`https://api.giphy.com/v1/gifs/random?api_key=${apiKey}`);
+
+peticion.then(resp => resp.json())
+    .then(({data}) => {
+        const {url} = data.images.original;
+
+        const img = document.createElement('img');
+        img.src = url;
+
+        document.body.append(img);
+        
+    })
+    .catch(
+        console.warn
+    );
+
